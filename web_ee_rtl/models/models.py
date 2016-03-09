12f7bb813fe8f292259779d5ca2993ade32aba84
Mohamed Magdy <mohamed.magdy@meunity.com>
2016-03-09 12:39:52 +0200

    [ADD] Inherit the class that is responsible for the getting and setting the 'Language Direction'.

--- web_ee_rtl/models/models.py
+++ web_ee_rtl/models/models.py
@@ -1,15 +1,50 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields, api
+from openerp import models
+import openerp
 
-# class web_ee_rtl(models.Model):
-#     _name = 'web_ee_rtl.web_ee_rtl'
+class QWeb(models.AbstractModel):
+    _name = 'ir.qweb'
+    _inherit = 'ir.qweb'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    def render(self, cr, uid, id_or_xml_id, qwebcontext=None, loader=None, context=None):
+        context = context or {}
+
+        if qwebcontext and qwebcontext.get('lang_direction', None):
+            return super(QWeb, self).render(cr, uid, id_or_xml_id, qwebcontext=qwebcontext, loader=loader, context=context)
+
+        lang_obj = self.pool.get('res.lang')
+        lang = context.get('lang', None)
+
+        if not lang:
+            if qwebcontext.get('lang', None):
+                lang = qwebcontext.get('lang')
+            elif uid:
+                user = self.pool.get('res.users').browse(cr, uid, uid, context=context)
+                lang = user.lang and user.lang or user.partner_id.lang
+            else:
+                lang = 'en_US'
+
+        lang_directions = lang_obj.get_language_dir(cr, uid, [], context=context)
+        lang_direction = lang_directions.get(lang, 'ltr')
+        qwebcontext['lang_direction'] = qwebcontext.get('lang_direction', None) or lang_direction
+
+        return super(QWeb, self).render(self, cr, uid, id_or_xml_id, qwebcontext=qwebcontext, loader=loader, context=context)
+
+
+class res_lang(models.Model):
+    _name = 'res.lang'
+    _inherit = 'res.lang'
+
+    @openerp.tools.ormcache(skiparg=3)
+    def _get_languages_dir(self, cr, uid, id, context=None):
+        ids = self.search(cr, uid, [('active', '=', True)], context=context)
+        langs = self.browse(cr, uid, ids, context=context)
+        return dict([(lg.code, lg.direction) for lg in langs])
+
+    def get_languages_dir(self, cr, uid, ids, context=None):
+        return self._get_languages_dir(cr, uid, ids)
+
+    def write(self, cr, uid, ids, vals, context=None):
+        self._get_languages_dir.clear_cache(self)
+        return super(res_lang, self).write(cr, uid, ids, vals, context)
